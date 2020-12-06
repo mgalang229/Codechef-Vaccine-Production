02d34c29ac4749c42bf7a015a503db1160c74e87
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-06 11:18:56 +0800

    Update sol.cpp

--- sol.cpp
+++ sol.cpp
@@ -1,27 +1,23 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
+
 using namespace std;
-using namespace __gnu_pbds;
-
-#define ll long long
-#define ar array
-
-typedef tree<int, null_type, less<int>, rb_tree_tag,
-tree_order_statistics_node_update> indexed_set;
 
 int main() {
-	ios::sync_with_stdio(0);
+	ios::sync_with_stdio(false);
 	cin.tie(0);
-	
-	int d1, d2, v1, v2, p;
+	int d1, v1, d2, v2, p;
 	cin >> d1 >> v1 >> d2 >> v2 >> p;
-	int tmp=0, cnt=0;
-	for(int i=1; tmp<p; ++i) {
-		if(i>=d1)
-			tmp+=v1;
-		if(i>=d2)
-			tmp+=v2;
+	int vaccine = 0;
+	int cnt = 0;
+	for (int i = 1; vaccine < p; i++) {
+		if (i >= d1) {
+			vaccine += v1;
+		}
+		if (i >= d2) {
+			vaccine += v2;
+		}
 		cnt++;
 	}
-	cout << cnt << "\n";
+	cout << cnt << '\n';
+	return 0;
 }
